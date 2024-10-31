3b1fde48b8961a8964847f428890ce6221295153
Vpekdas <volkan.pekdas@hotmail.com>
2024-10-31 14:08:39 +0100

    tryting to display a template php in nginx

--- srcs/requirements/mariadb/tools/initialize-db.sh
+++ srcs/requirements/mariadb/tools/initialize-db.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-service mariadb start;
+if [[ ! -d "/var/lib/mysql/$SQL_DATABASE" ]]; then
+    mariadb-install-db --user=mysql
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+    service mariadb start
 
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+    mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
 
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+    mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+    mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
-mysql -e "FLUSH PRIVILEGES;"
+    mysql -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+    service mariadb stop
+fi
 
-exec mysqld_safe
+exec "$@"

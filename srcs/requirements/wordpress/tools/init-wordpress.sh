cd391ddee07b712d27bdc08b66decd6840eb7acc
Vpekdas <volkan.pekdas@hotmail.com>
2024-11-01 21:13:07 +0100

    feat: Adminer Integration with Nginx
    - Added a location block for Adminer in the Nginx configuration.
    - Configured the initialization script to start PHP-FPM and Nginx.
    - Ensured Adminer is accessible via port 80.
    - Set appropriate permissions for Adminer files.
    - Downloaded the latest version of Adminer.

--- srcs/requirements/wordpress/tools/init-wordpress.sh
+++ srcs/requirements/wordpress/tools/init-wordpress.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-FILE=/var/www/wordpress/wp-config.php
-
-redis-server --protected-mode no &
+FILE=/var/www/html/wp-config.php
 
 # Check if the wp-config.php file does not exist
 if [ ! -f "$FILE" ]; then
 
-    mkdir -p /var/www/wordpress
-    wp core download --allow-root --path="/var/www/wordpress"
+    mkdir -p /var/www/html
+    wp core download --allow-root --path="/var/www/html"
 
     # SInce CLI doest not work, we are finding and repacll
-    cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
-    sed -i -e "s/database_name_here/$SQL_DATABASE/g" /var/www/wordpress/wp-config.php
-    sed -i -e "s/username_here/$SQL_USER/g" /var/www/wordpress/wp-config.php
-    sed -i -e "s/password_here/$SQL_PASSWORD/g" /var/www/wordpress/wp-config.php
-    sed -i -e "s/localhost/mariadb/g" /var/www/wordpress/wp-config.php
+    cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+    sed -i -e "s/database_name_here/$SQL_DATABASE/g" /var/www/html/wp-config.php
+    sed -i -e "s/username_here/$SQL_USER/g" /var/www/html/wp-config.php
+    sed -i -e "s/password_here/$SQL_PASSWORD/g" /var/www/html/wp-config.php
+    sed -i -e "s/localhost/mariadb/g" /var/www/html/wp-config.php
 
     # Install WordPress using WP-CLI
     wp core install --allow-root \
@@ -23,24 +21,24 @@ if [ ! -f "$FILE" ]; then
     --title="$WP_TITLE" \
     --admin_user=$WP_ADMIN_USER \
     --admin_password=$WP_ADMIN_PASSWORD \
-    --admin_email=$WP_ADMIN_EMAIL --path='/var/www/wordpress'
+    --admin_email=$WP_ADMIN_EMAIL --path='/var/www/html'
 
-    wp --allow-root user create guest guest@example.com --user_pass=password123 --path='/var/www/wordpress'
+    wp --allow-root user create guest guest@example.com --user_pass=password123 --path='/var/www/html'
 
-    wp config set WP_REDIS_HOST redis --allow-root --path='/var/www/wordpress'
-    wp config set WP_REDIS_PORT 6379 --raw --allow-root --path='/var/www/wordpress'
-    wp config set WP_CACHE_KEY_SALT $WP_URL --allow-root --path='/var/www/wordpress'
-    wp config set WP_REDIS_CLIENT phpredis --allow-root --path='/var/www/wordpress'
+    wp config set WP_REDIS_HOST redis --allow-root --path='/var/www/html'
+    wp config set WP_REDIS_PORT 6379 --raw --allow-root --path='/var/www/html'
+    wp config set WP_CACHE_KEY_SALT $WP_URL --allow-root --path='/var/www/html'
+    wp config set WP_REDIS_CLIENT phpredis --allow-root --path='/var/www/html'
     
-	wp plugin install redis-cache --activate --allow-root --path='/var/www/wordpress'
-    wp plugin update --all --allow-root --path='/var/www/wordpress'
-	wp redis enable --allow-root --path='/var/www/wordpress'
+	wp plugin install redis-cache --activate --allow-root --path='/var/www/html'
+    wp plugin update --all --allow-root --path='/var/www/html'
+	wp redis enable --allow-root --path='/var/www/html'
 
 
 fi
 
 # Ensure correct permissions
-chown -R www-data:www-data /var/www/wordpress
+chown -R www-data:www-data /var/www/html
 
 # Start PHP-FPM in the foreground
 exec "$@"
